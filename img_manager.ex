6212c43f543801196b3b7ce90fe1cdebd8162edb
Simon Tirant <simon.tirant@gmail.com>
2021-09-09 19:15:14 +0200

    bug resolutions and error management

--- img_manager.ex
+++ img_manager.ex
@@ -3,33 +3,36 @@ defmodule ImgManager do
         path = IO.gets(~s[Enter the path of the folder you want to manage : (type "-q" to quit the tool)\n])
             |> String.trim
 
-        if path == "-q" do
+        if path === "-q" do
            "Goodbye !"
         else
             if File.exists?(path) and File.dir?(path) do
-                folder = IO.gets("Name the folder we will create to store your images : ")
+                folder = IO.gets(~s[Name the folder that will be created to store your images : (type "-r" to go back to previous step)\n])
                     |> String.trim
 
+                if folder === "-r" do
+                    start()
+                else
+                    File.cd(path)
 
-                File.cd(path)
-
-                if !File.exists?(folder), do: File.mkdir(folder)
-                
-                File.ls()
-                |> Kernel.elem(1)
-                |> Enum.each(fn x -> 
-                    if String.ends_with?(x, [".jpg", ".png", ".gif", ".bmp"]) do
-                        File.cp(x, "./#{folder}/" <> x)
-                        File.rm(x)
-                    end
-                end)
-                
-                IO.puts(~s[Image files succefully moved to "#{folder}" folder !\n])
+                    if !File.exists?(folder), do: File.mkdir(folder)
+                    
+                    File.ls()
+                    |> Kernel.elem(1)
+                    |> Enum.each(fn x -> 
+                        if String.ends_with?(x, [".jpg", ".png", ".gif", ".bmp"]) do
+                            File.cp(x, "./#{folder}/" <> x)
+                            File.rm(x)
+                        end
+                    end)
+                    
+                    IO.puts(~s[\nImage files succefully moved to "#{folder}" folder !\n])
+                    start()
+                end
             else
                 IO.puts(~s[The path "#{path}" doesn't exists on your computer.\n])
+                start()
             end
-
-            start()
         end
     end
 end
